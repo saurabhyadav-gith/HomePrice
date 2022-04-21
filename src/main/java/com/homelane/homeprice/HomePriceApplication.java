21c6c2ddd5460fc0c2f62ebb25dca387133fc604
Saurabh Yadav <saurabhyadav970@gmail.com>
2022-04-22 00:32:25 +0530

    Intial skeleton commit

---
+++ src/main/java/com/homelane/homeprice/HomePriceApplication.java
@@ -0,0 +1,13 @@
+package com.homelane.homeprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomePriceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomePriceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/homelane/homeprice/HomePriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homelane.homeprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomePriceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
